2cd27a0534660ec0833936ba79b23dbd7d11bad6
Sergio Blahkhelm DeLeón <codework@Sergios-MBP.lan>
2022-06-19 17:08:11 -0500

    Updated linting and formatting files for project

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="app-logo">PictoSplash</h1>
-      </header>
-
-      <input className="SearchBar" placeholder="Search for new pictures here" />
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1 className="app-logo">PictoSplash</h1>
+            </header>
+            <input className="SearchBar" placeholder="Search for new pictures here" />
+        </div>
+    );
 }
 
 export default App;
